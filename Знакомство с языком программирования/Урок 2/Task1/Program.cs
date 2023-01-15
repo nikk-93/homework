685d5017bb180844418483457173ee50482a6686
NILadygin <nikk-93@mail.ru>
2023-01-15 19:42:00 +0500

    Реализованы задачи по Урок 2: Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
    456 -> 5
    782 -> 8
    918 -> 1
    
    Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
    
    645 -> 5
    
    78 -> третьей цифры нет
    
    32679 -> 6
    
    Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
    
    6 -> да
    7 -> да
    1 -> нет

---
+++ Знакомство с языком программирования/Урок 2/Task1/Program.cs
@@ -0,0 +1,55 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /////
+        // Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+        // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+        /////
+        int number = new Random().Next(1, 200);
+        Console.WriteLine("Число {0}: ", number);
+        Console.Write("Какую цифру по порядку показать: ");
+        int position = int.Parse(Console.ReadLine() ?? "0");
+
+        int leftDig = GetDigitNumLeft(position, number);
+        int rightDig = GetDigitNumRight(position, number);
+
+        if (leftDig == -1)
+            Console.WriteLine("Цифра слева не найдена");
+        else
+            Console.WriteLine("Цифра слева: {0}", leftDig);
+
+        if (rightDig == -1)
+            Console.WriteLine("Цифра справа не найдена");
+        else
+            Console.WriteLine("Цифра справа: {0}", rightDig);
+    }
+
+    public static int GetDigitNumLeft(int position, int number)
+    {
+        var digits = GetDigitsNumber(number);
+        return digits.Count - position > -1 ? digits[digits.Count - position] : -1;
+    }
+
+    public static int GetDigitNumRight(int position, int number)
+    {
+        var digits = GetDigitsNumber(number);
+        return digits.Count - position > -1 ? digits[position - 1] : -1;
+    }
+
+    public static List<int> GetDigitsNumber(int number)
+    {
+        int num = number;
+        List<int> digits = new List<int>();
+        int modnum;
+
+        do
+        {
+            modnum = num % 10;
+            digits.Add(modnum);
+            num /= 10;
+        } while (num > 0);
+
+        return digits;
+    }
+}
---
+++ Знакомство с языком программирования/Урок 2/Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /////
+        // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+        /////
+        Dictionary<int, string> weekDays = new Dictionary<int, string>()
+        {
+            { 0, "Понедельник" },
+            { 1, "Вторник" },
+            { 2, "Среда" },
+            { 3, "Четверг" },
+            { 4, "Пятница" },
+            { 5, "Суббота" },
+            { 6, "Воскресенье" },
+        };
+
+        Dictionary<int, string> weekEnds = new Dictionary<int, string>()
+        {
+            { 5, "Суббота" },
+            { 6, "Воскресенье" },
+        };
+
+        Console.Write("Введите номер дня недели: ");
+        int numWeekDay = int.Parse(Console.ReadLine() ?? "0") - 1;
+        weekDays.TryGetValue(numWeekDay, out string? nameWeekDay);
+
+        if (nameWeekDay != null)
+        {
+            Console.WriteLine(nameWeekDay);
+            weekEnds.TryGetValue(numWeekDay, out string? nameWeekEndDay);
+            Console.WriteLine(nameWeekEndDay != null ? "выходной" : "не выходной");
+        }
+        else
+            Console.WriteLine("Нет такого дня");
+    }
+}
